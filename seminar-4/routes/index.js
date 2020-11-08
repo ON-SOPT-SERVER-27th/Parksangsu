977386ec16fdd92ddf577582c93b009f36080da5
epitone <epitoneplus@gmail.com>
2020-11-08 23:37:06 +0900

    signup post routes problem is resolved

--- seminar-4/routes/index.js
+++ seminar-4/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/users', require('./users'));
+router.use('/users', require('./users/index'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
