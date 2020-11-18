753c442e0dbe5976f8d7c6ceed97ad88fa9da1bf
epitone <epitoneplus@gmail.com>
2020-11-18 21:07:19 +0900

    feat: assignment README add 세미나 과제 정리 리드미 작성

--- homework/week2/level2/main.js
+++
@@ -1,8 +0,0 @@
-const calculator = require('./calculator');
-
-console.log(
-    calculator.add(1, 2), 
-    calculator.substract(2, 1), 
-    calculator.multiply(2, 2), 
-    calculator.Divide(4, 2)
-);
