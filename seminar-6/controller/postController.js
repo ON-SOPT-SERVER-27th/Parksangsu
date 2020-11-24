2c151d7b4f54e168ee643a48b89d46a3d0ed07c6
epitone <epitoneplus@gmail.com>
2020-11-25 00:06:01 +0900

    update: post service 계층 분리 postController, postService 계층 분리 마무리

--- seminar-6/controller/postController.js
+++ seminar-6/controller/postController.js
@@ -8,19 +8,16 @@ const postService = require('../service/postService');
 module.exports = {
     createPost: async (req, res) => {
         const { title, contents, userId } = req.body;
-        
         if(!title || !contents || !userId){
             console.log('원하는 값을 넣어주세요.');
             return res.status(sc.BAD_REQUEST).send(ut.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
         }
-
         try {
             const userIdCheck = await userService.userIdCheck(userId);
             if(!userIdCheck) {
                 console.log('유저 id가 없습니다');
                 return res.status(sc.BAD_REQUEST).send(ut.fail(sc.BAD_REQUEST, rm.NULL_VALUE));
             }
-            
             const postCreate = await postService.postCreate(userId, title, contents);
             return res.status(sc.OK).send(ut.success(sc.OK, rm.CREATE_POST_SUCCESS, postCreate));
         } catch(error) {
@@ -30,19 +27,9 @@ module.exports = {
     },
     readAllPosts: async (req, res) => {
         try {
-            const posts = await Post.findAll({
-                attributes: ['title', 'contents'],
-                include: [{
-                    model: User,
-                    attributes: ['id', 'userName', 'email']
-                }, {
-                    model: User,
-                    as: 'Liker',
-                    attributes: { exclude : ['password', 'salt', 'id', 'email' ]}
-                }]
-            });
-            console.log(JSON.stringify(posts, null, 2));
-            return res.status(sc.OK).send(ut.success(sc.OK, rm.READ_POST_ALL_SUCCESS, posts));
+            const getReadAllUsers = await postService.postReadAll();
+            // console.log(JSON.stringify(getReadAllUsers, null, 2));
+            return res.status(sc.OK).send(ut.success(sc.OK, rm.READ_POST_ALL_SUCCESS, getReadAllUsers));
         } catch (err) {
             return res.status(sc.INTERNAL_SERVER_ERROR).send(ut.fail(sc.INTERNAL_SERVER_ERROR, rm.READ_POST_ALL_FAIL));
         }
@@ -51,7 +38,7 @@ module.exports = {
         const PostId = req.params.postId;
         const UserId = req.body.userId;
         try {
-            const like = await Like.create({ PostId, UserId });
+            const like = await postService.likeCreate(PostId, UserId)
             return res.status(sc.OK).send(ut.success(sc.OK, rm.CREATE_LIKE_SUCCESS, like));
         } catch (err) {
             console.log(err);
@@ -63,24 +50,14 @@ module.exports = {
         const UserId = req.body.userId;
 
         try {
-            const like = await Like.findOne({
-                where: {
-                    PostId,
-                    UserId
-                }
-            })
+            const like = await postService.likeFindById(PostId, UserId);
             
             if(!like) {
                 console.log('존재하지 않는 id입니다.');
                 return res.status(sc.BAD_REQUEST).send(ut.fail(sc.BAD_REQUEST, rm.DELETE_LIKE_FAIL));
             }
 
-            const likeDelete = await Like.destroy({
-                where: {
-                    PostId,
-                    UserId
-                }
-            })
+            const likeDelete = await postService.likeDelete(PostId, UserId);
             res.status(sc.OK).send(ut.success(sc.OK, rm.DELETE_USER_SUCCESS, likeDelete));
         } catch (err) {
             console.log(err);
--- seminar-6/service/postService.js
+++ seminar-6/service/postService.js
@@ -1,4 +1,4 @@
-const { User, Post } = require('../models');
+const { User, Post, Like } = require('../models');
 
 module.exports = {
     postCreate: async (userId, title, contents) => {
@@ -12,7 +12,73 @@ module.exports = {
         } catch (err) {
             throw err;
         }
+    },
+    uploadImage: async (title, contents, image) => {
+        try {
+            const uploadImage = await Post.create({
+                postImageUrl: image,
+                title,
+                contents
+            })
+            return uploadImage;
+        } catch (err) {
+            throw err;
+        }
+    },
+    postReadAll: async () => {
+        try {
+            const readAllPost = await Post.findAll({
+                attributes: ['title', 'contents'],
+                include: [{
+                    model: User,
+                    attributes: ['id', 'userName', 'email']
+                }, {
+                    model: User,
+                    as: 'Liker',
+                    attributes: { exclude : ['password', 'salt', 'id', 'email' ]}
+                }]
+            })
+            return readAllPost;
+        } catch (err) {
+            throw err;
+        }
+    },
+    likeFindById: async (PostId, UserId) => {
+        try{
+            const findByIdLike = await Like.findOne({
+                where: {
+                    PostId,
+                    UserId
+                }
+            })
+            return findByIdLike;
+        } catch (err) {
+            throw err;
+        }
+    },
+    likeCreate: async (PostId, UserId) => {
+        try {
+            const createLike = await Like.create({
+                PostId,
+                UserId
+            })
+            return createLike;
+        } catch (err) {
+            throw err;
+        }
+    },
+    likeDelete: async (PostId, UserId) => {
+        try {
+            const deleteLike = await Like.destroy({
+                where: {
+                    PostId,
+                    UserId
+                }
+            })
+            return deleteLike;
+        } catch (err) {
+            throw err;
+        }
     }
 }
-
 
